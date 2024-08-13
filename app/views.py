4d0abc4edf973b10b88a5a01e1f79f3ccca507db
stagib <5Pw57u0Tf@protonmail.com>
2024-08-14 07:59:29 +1000

    cat

--- app/views.py
+++ app/views.py
@@ -1,7 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+import requests
 
 views = Blueprint("views", __name__)
 
 @views.route("/")
 def index():
-  return "index"
+  return render_template("index.html")
+
+@views.route("/cat", methods=["GET"])
+def get_cat():
+    response = requests.get("https://cataas.com/cat")
+    if response.status_code == 200:
+        cat_image_url = "https://cataas.com/cat"
+    else:
+        cat_image_url = None
+    return

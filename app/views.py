c862d3c13838a203daf94f56dd8db8b9c2bbfffd
stagib <5Pw57u0Tf@protonmail.com>
2024-08-14 13:19:14 +1000

    ui changes and different cats

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
-import requests
+import random
+
+from flask import Blueprint, render_template, request
 
 views = Blueprint("views", __name__)
 
@@ -9,9 +10,8 @@ def index():
 
 @views.route("/cat", methods=["GET"])
 def get_cat():
-    response = requests.get("https://cataas.com/cat")
-    if response.status_code == 200:
-        cat_image_url = "https://cataas.com/cat"
-    else:
-        cat_image_url = None
-    return
+    q = request.args.get("q")
+    if not q:
+       q = 1
+    cat_image_url = "https://cataas.com/cat?" + str(random.randint(1, 10000))
+    return render_template("cat.html", cat_image_url=cat_image_url)
